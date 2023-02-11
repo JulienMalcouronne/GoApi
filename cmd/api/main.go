f74e9345e4351a9f875142d906bd2cabcaf16ef4
Julien Malcouronne <malcouronnejulien@gmail.com>
2023-02-12 00:06:10 +0100

    feat: prepare route guard

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -16,10 +16,11 @@ type config struct {
 }
 
 type application struct {
-	config   config
-	infoLog  *log.Logger
-	errorLog *log.Logger
-	models   data.Models
+	config      config
+	infoLog     *log.Logger
+	errorLog    *log.Logger
+	models      data.Models
+	environment string
 }
 
 func init() {
@@ -39,6 +40,7 @@ func main() {
 	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
 	dsn := os.Getenv("DSN")
+	environment := os.Getenv("ENV")
 
 	db, err := driver.ConnectPostgres(dsn)
 	if err != nil {
@@ -47,10 +49,11 @@ func main() {
 	defer db.SQL.Close()
 
 	app := &application{
-		config:   cfg,
-		infoLog:  infoLog,
-		errorLog: errorLog,
-		models:   data.New(db.SQL),
+		config:      cfg,
+		infoLog:     infoLog,
+		errorLog:    errorLog,
+		models:      data.New(db.SQL),
+		environment: environment,
 	}
 
 	err = app.serve()
